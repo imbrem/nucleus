f6ee0b302d67c6b8947caa4153d3b2934c3ba70d
Jad Ghalayini <jad@tekne.dev>
2026-07-27 17:56:20 +0100

    Support secure randomness in JavaScript Wasm hosts

--- crates/lib/rand/src/lib.rs
+++ crates/lib/rand/src/lib.rs
@@ -2,3 +2,17 @@
 
 pub use rand;
 pub use rand::*;
+
+#[cfg(all(test, target_arch = "wasm32", target_os = "unknown"))]
+mod tests {
+    use super::random;
+    use wasm_bindgen_test::wasm_bindgen_test;
+
+    #[wasm_bindgen_test]
+    fn javascript_host_supplies_randomness() {
+        let first = random::<[u8; 32]>();
+        let second = random::<[u8; 32]>();
+
+        assert_ne!(first, second);
+    }
+}
