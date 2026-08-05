6ddb24889c9f192785519aa99016f35ff829dd84
Jad Ghalayini <jad@tekne.dev>
2026-08-05 07:30:07 +0100

    Drive HOL guests from caller-owned REPL state

--- crates/repl/src/lib.rs
+++ crates/repl/src/lib.rs
@@ -17,7 +17,12 @@ pub mod hol_recipes;
 #[cfg(not(target_arch = "wasm32"))]
 mod hol_guest;
 #[cfg(not(target_arch = "wasm32"))]
-pub use hol_guest::{HolGuestError, run_hol_proof_component};
+pub use covalence_proton::WasmtimeComponentLimits;
+#[cfg(not(target_arch = "wasm32"))]
+pub use hol_guest::{
+    HolGuestError, ManagedHolGuestError, ManagedHolGuestResult, retain_signed_hol_guest_artifact,
+    run_hol_proof_component, run_managed_hol_proof_component,
+};
 
 pub use covalence_nucleus::sql::{
     ImageError, MAX_IMAGE_BYTES, Outcome, QueryResult, Statement, Value,
