4854b63ef16ef9dcea202e06b049853fd3adbaef
Jad Ghalayini <jad@tekne.dev>
2026-08-26 10:53:43 +0100

    lean: decode classical lambda realizations (#1052)

--- lean/Nucleus/Nucleus/Hol/Ethane/Arena/OneBased/NaturalPackageSemantics.lean
+++ lean/Nucleus/Nucleus/Hol/Ethane/Arena/OneBased/NaturalPackageSemantics.lean
@@ -1,6 +1,7 @@
 import Nucleus.Hol.Ethane.Arena.OneBased.DenseKernelTransport
 import Nucleus.HolE.ClassicalNaturals
 import Nucleus.HolE.ClassicalEqualityRealization
+import Nucleus.HolE.ClassicalLambdaRealization
 
 /-!
 # Semantic certificates for checked natural-number packages
